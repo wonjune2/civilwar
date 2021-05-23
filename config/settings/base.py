adf7acc5d5338803bc051542b5fd91f4d9513163
이원준 <zzzdnjswns@naver.com>
2021-05-24 06:15:26 +0900

    asda

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,18 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, "secrets.json")
-
-with open(secret_file) as f:
+with open("secrets.json") as f:
     secrets = json.loads(f.read())
-
+ 
+ 
+# Keep secret keys in secrets.json
 def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "Set the {0} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
-
+ 
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common.apps.CommonConfig',
-
 ]
 
 MIDDLEWARE = [
