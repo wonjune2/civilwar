645265c60cd55bed247bf93ad0ec368ac079bb00
이원준 <zzzdnjswns@naver.com>
2021-05-24 05:47:19 +0900

    EE

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ import os, json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common.apps.CommonConfig',
+    
 ]
 
 MIDDLEWARE = [
