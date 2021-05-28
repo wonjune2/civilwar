0ef5766f282395616075283d8d6af103338287f3
leewonJoon <dldnjswns111@gmail.com>
2021-05-28 15:56:25 +0900

    멤버 수정 중

--- static/name-modify.js
+++ static/name-modify.js
@@ -1,24 +1,20 @@
 var main = {
   init: function () {
     var _this = this;
-    this.targets;
-    var par;
+    var oldName;
+    var oldInput;
     $(".name-modify").on("click", function (e) {
-      _this.modify(e);
+      console.log(oldName);
+      //   _this.cancel();
+      _this.modify(e, oldName, oldInput);
     });
   },
-  modify: function (e) {
-    var par = $(e.target).parent();
-    var oldTag = $(e.target).parent().prev().html();
-    par = par
-      .prev()
-      .html(
-        '<input type="text" class="form-control" placeholder="이름을 적어주세요" aria-label="Username" aria-describedby="addon-wrapping">'
-      )
-      .children()
-      .html();
-    console.log("par : ", par, "oldTag : ", oldTag);
-    return oldTag, par;
+  modify: function (e, oldName, oldInput) {
+    var targets = $(e.target).parent().prev();
+    oldName = targets.html();
+    oldInput = targets.html(
+      '<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">'
+    );
   },
 };
 main.init();
