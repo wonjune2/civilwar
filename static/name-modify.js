86561836605d8cdfa22c6bbb0d043b58f2b82468
leewonJoon <dldnjswns111@gmail.com>
2021-05-28 17:12:03 +0900

    멤버 화면 수정중

--- static/name-modify.js
+++ static/name-modify.js
@@ -1,20 +1,32 @@
 var main = {
   init: function () {
     var _this = this;
-    var oldName;
-    var oldInput;
+    var test;
     $(".name-modify").on("click", function (e) {
-      console.log(oldName);
       //   _this.cancel();
-      _this.modify(e, oldName, oldInput);
+      if (test) _this.cancel(test);
+      test = _this.modify(e);
+    });
+    $(".cancelBtn").on("click", function (e) {
+      _this.btnCancel(e);
     });
   },
-  modify: function (e, oldName, oldInput) {
+  modify: function (e) {
     var targets = $(e.target).parent().prev();
-    oldName = targets.html();
-    oldInput = targets.html(
+    console.log("targets:", targets);
+    var oldName = targets.html();
+    targets.html(
       '<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">'
     );
+    var btntarget = $(e.target).parent();
+    console.log(btntarget.text());
+    btntarget.html(
+      '<button type="button" class="btn btn-outline-dark btn-sm cancelBtn">취소</button>'
+    );
+    return { oldName: oldName, targets: targets, btntarget: btntarget };
+  },
+  cancel: function (test) {
+    test.targets.html(test.oldName);
   },
 };
 main.init();
